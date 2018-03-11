984436819e588813dc3448a181ac93c61b405fb8
Chase <chaseit12@gmail.com>
2018-03-11 19:27:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,9 +31,7 @@ class Song
   
   def self.find_by_name(song_name)
     self.all.find{ |song|
-      if song.name == song_name
-           
-      end
+      song.name == song_name
     }
   end
 end
